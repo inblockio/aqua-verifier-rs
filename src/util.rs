c4d7f08705ed0bf4db19c1be1f312d020dd86f8d
arthur-kamau <kamaukenn11@gmail.com>
2024-11-05 17:39:05 +0300

    small fix

--- src/util.rs
+++ src/util.rs
@@ -7,7 +7,7 @@ use crate::model::{
     ResultStatus, ResultStatusEnum, RevisionAquaChainResult, RevisionVerificationResult,
 };
 
-pub fn verifyRevision(revision: Revision) -> RevisionVerificationResult {
+pub fn verify_revision(revision: Revision) -> RevisionVerificationResult {
 
     let default_result_status: ResultStatus = ResultStatus {
         status: ResultStatusEnum::MISSING,
@@ -15,7 +15,7 @@ pub fn verifyRevision(revision: Revision) -> RevisionVerificationResult {
         message: "default".to_string()
     };
 
-    let revisionResult: RevisionVerificationResult = RevisionVerificationResult {
+    let revision_result: RevisionVerificationResult = RevisionVerificationResult {
         successful: false,
         file_verification: default_result_status.clone(),
         content_verification: default_result_status.clone(),
@@ -26,42 +26,42 @@ pub fn verifyRevision(revision: Revision) -> RevisionVerificationResult {
     };
 
 
-  return revisionResult;   
+  return revision_result;   
 }
 
-pub fn verifySignature(
+pub fn verify_signature(
     signature: RevisionSignature,
     previous_verification_hash: String,
 ) -> ResultStatus {
-    let mut defaultResultStatus: ResultStatus = ResultStatus {
+    let mut default_result_status: ResultStatus = ResultStatus {
         status: ResultStatusEnum::MISSING,
         successful: false,
         message: "default".to_string(),
     };
 
-    return defaultResultStatus;
+    return default_result_status;
 }
 
-pub fn verifyWitness(
+pub fn verify_witness(
     witness: RevisionWitness,
     verification_hash: String,
-    doVerifyMerkleProof: bool,
-    alchemyKey: String,
-    doAlchemyKeyLookUp: bool,
+    do_verify_merkle_proof: bool,
+    alchemy_key: String,
+    do_alchemy_key_look_up: bool,
 ) -> ResultStatus {
-    let mut defaultResultStatus: ResultStatus = ResultStatus {
+    let mut default_result_status: ResultStatus = ResultStatus {
         status: ResultStatusEnum::MISSING,
         successful: false,
         message: "default".to_string(),
     };
 
-    return defaultResultStatus;
+    return default_result_status;
 }
 
 pub fn verifyAquaChain(
-    aquaChain: HashChain,
-    alchemyKey: String,
-    doAlchemyKeyLookUp: bool,
+    aqua_chain: HashChain,
+    alchemy_key: String,
+    do_alchemy_key_look_up: bool,
 ) -> RevisionAquaChainResult {
     let mut revisionResultsData: Vec<RevisionVerificationResult> = Vec::new();
 
@@ -73,6 +73,6 @@ pub fn verifyAquaChain(
     return hashChainResult;
 }
 
-pub fn signFile() {}
+pub fn sign_aqua_chain() {}
 
-pub fn witnessFile() {}
+pub fn witness_aqua_chain() {}
